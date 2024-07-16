33640a7a27f9745a09ebe79be76c8ab9ade7791a
Joseph Levy <josephlevy222@gmail.com>
2024-07-16 09:45:10 -0700

    Initial Commit

---
+++ RichTextKitForAttributedString/RichTextKitForAttributedStringApp.swift
@@ -0,0 +1,17 @@
+//
+//  RichTextKitForAttributedStringApp.swift
+//  RichTextKitForAttributedString
+//
+//  Created by Joseph Levy on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct RichTextKitForAttributedStringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
